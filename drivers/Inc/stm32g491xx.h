b5b28f4589d508427e1baf6c80412c704860c3a3
Paweł <pawel_smusz@yahoo.com>
2026-08-13 20:45:22 +0200

    Base peripherals addresses.

--- drivers/Inc/stm32g491xx.h
+++ drivers/Inc/stm32g491xx.h
@@ -5,9 +5,77 @@
  *      Author: Paweł Smusz
  */
 
+
 #ifndef INC_STM32G491XX_H_
 #define INC_STM32G491XX_H_
 
+/*
+ * base addresses of Flash and SRAM memories
+ */
+
+#define FLASH_BASEADDR					0x08000000U
+#define SRAM1_BASEADDR					0x20000000U
+#define SRAM2_BASEADDR					0x20014000U
+#define ROM								0x1FFF8000U
+#define SRAM 							SRAM1_BASEADDR
+
+
+/*
+ * AHBx and APBx Bus Peripheral base addresses
+ */
+
+#define PERIPH_BASE						0x40000000U
+#define APB1PERIPH_BASE					PERIPH_BASE
+#define APB2PERIPH_BASE					0x40010000U
+#define AHB1PERIPH_BASE					0x40020000U
+#define AHB2PERIPH_BASE					0x48000000U
+
+/*
+ * Base addresses of peripherals which are hanging on AHB1 bus
+ */
+
+
+
+/*
+ * Base addresses of peripherals which are hanging on AHB2 bus
+ */
+
+#define GPIOA_BASEADDR					(AHB2PERIPH_BASE + 0x0000)
+#define GPIOB_BASEADDR					(AHB2PERIPH_BASE + 0x0400)
+#define GPIOC_BASEADDR					(AHB2PERIPH_BASE + 0x0800)
+#define GPIOD_BASEADDR					(AHB2PERIPH_BASE + 0x0C00)
+#define GPIOE_BASEADDR					(AHB2PERIPH_BASE + 0x1000)
+#define GPIOF_BASEADDR					(AHB2PERIPH_BASE + 0x1400)
+#define GPIOG_BASEADDR					(AHB2PERIPH_BASE + 0x1800)
+
+/*
+ * Base addresses of peripherals which are hanging on APB1 bus
+ */
+
+#define I2C1_BASEADDR					(APB1PERIPH_BASE + 0x5400)
+#define I2C2_BASEADDR					(APB1PERIPH_BASE + 0x5800)
+#define I2C3_BASEADDR					(APB1PERIPH_BASE + 0x7800)
+#define I2C4_BASEADDR					(APB1PERIPH_BASE + 0x8400)
+
+#define SPI2_BASEADDR					(APB1PERIPH_BASE + 0x3800)
+#define SPI3_BASEADDR					(APB1PERIPH_BASE + 0x3C00)
+
+#define USART2_BASEADDR					(APB1PERIPH_BASE + 0x4400)
+#define USART3_BASEADDR					(APB1PERIPH_BASE + 0x4800)
+#define UART4_BASEADDR					(APB1PERIPH_BASE + 0x4C00)
+#define UART5_BASEADDR					(APB1PERIPH_BASE + 0x5000)
+
+/*
+ * Base addresses of peripherals which are hanging on APB2 bus
+ */
+
+#define EXTI_BASEADDR					(APB2PERIPH_BASE + 0x0400)
+#define SYSCFG_BASEADDR					(APB2PERIPH_BASE + 0x0000)
+
+#define SPI1_BASEADDR					(APB2PERIPH_BASE + 0x3000)
+#define SPI4_BASEADDR					(APB2PERIPH_BASE + 0x3C00)
+
+#define USART1_BASEADDR					(APB2PERIPH_BASE + 0x3800)
 
 
 #endif /* INC_STM32G491XX_H_ */
