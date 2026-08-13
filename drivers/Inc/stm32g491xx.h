4f217233fceee2c6ae0dfda50bdf9afe09eba936
Paweł <pawel_smusz@yahoo.com>
2026-08-13 18:59:59 +0200

    Initial project

---
+++ drivers/Inc/stm32g491xx.h
@@ -0,0 +1,13 @@
+/*
+ * stm32g491xx.h
+ *
+ *  Created on: 13 sie 2026
+ *      Author: Paweł Smusz
+ */
+
+#ifndef INC_STM32G491XX_H_
+#define INC_STM32G491XX_H_
+
+
+
+#endif /* INC_STM32G491XX_H_ */
