53cb02c7b3e9f063f780b8b9f95f754ab4a9653b
Paweł <pawel_smusz@yahoo.com>
2026-08-16 16:06:11 +0200

    Modeling control structure for GPIO pin

---
+++ drivers/Inc/stm32g491xx_gpio_driver.h
@@ -0,0 +1,33 @@
+/*
+ * stm32g491xx_gpio_driver.h
+ *
+ *  Created on: 16 sie 2026
+ *      Author: Paweł Smusz
+ */
+
+#ifndef INC_STM32G491XX_GPIO_DRIVER_H_
+#define INC_STM32G491XX_GPIO_DRIVER_H_
+
+#include "stm32g491xx.h"
+
+
+typedef struct {
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinOPType;
+	uint8_t GPIO_PinAltFunMode;
+}GPIO_PinConfig_t;
+
+/*
+ * This is a Handle structure for a GPIO pin
+ */
+
+typedef struct
+{
+	GPIO_RegDef_t *pGPIOx;  /* This hold the base address of the GPIO port to which the pin belongs */
+	GPIO_PinConfig_t GPIO_PinConfig; /* This holds GPIO pin configuration settings */
+}GPIO_Handle_t;
+
+#endif /* INC_STM32G491XX_GPIO_DRIVER_H_ */
---
+++ drivers/Src/stm32g491xx_gpio_driver.c
@@ -0,0 +1,8 @@
+/*
+ * stm32g491xx_gpio_driver.c
+ *
+ *  Created on: 16 sie 2026
+ *      Author: Paweł Smusz
+ */
+
+#include "stm32g491xx_gpio_driver.h"
